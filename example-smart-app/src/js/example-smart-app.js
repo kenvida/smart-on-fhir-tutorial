ed58ecac4348aa58a60b68fae9724f23e66e5a80
kenvida <30702009+kenvida@users.noreply.github.com>
2017-08-06 15:40:09 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -18,20 +18,18 @@ var observationJSON = { "list" : []  };
         var patient = smart.patient;
      
         var pt = patient.read();
-        /*
+      
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4','http://loinc.org|3094-0','http://loinc.org|1920-8']
                       }
                     }
                   });
-        */
-        var obv = smart.patient.api.fetchAll({type: 'Observation'});
-        
+       
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
